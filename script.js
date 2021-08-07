32303a2f3f5c4cd7952681e94c8db312cd53ab76
Nikita <chip_05@inbox.ru>
2021-08-07 16:02:22 +0300

    update layout

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-AOS.init();
+var links = document.getElementsByTagName("a")
+for (let i of links) {
+    if (i.href.startsWith("https://salebot.site/r/")) {
+        if (i.href.split("?").length > 1) {
+            i.href += "&" + location.search.split("?")[1]
+        } else {
+            i.href += location.search
+        }
+    }
+}
+
+var links = document.getElementsByTagName("a")
+var cookies = ['_fbc', '_fbp', '_ga', '_ym_uid', 'roistat_visit']
+var cookies_string = ''
+cookies.forEach(function (cookie_name) {
+    let cookie_val = get_cookie(cookie_name);
+    if (cookie_val !== undefined) {
+        cookies_string += (cookies_string != '' ? "&" : '') + cookie_name + "='" + cookie_val + "'";
+    }
+})
+for (let i of links) {
+    if (i.href.startsWith("https://salebot.site/r/")) {
+        if (i.href.split("?").length > 1) {
+            i.href += "&" + cookies_string
+        } else {
+            i.href += "?" + cookies_string
+        }
+    }
+}
+function get_cookie(name) {
+    let matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
--- script/script.dev.js
+++ script/script.dev.js
@@ -1,3 +1,79 @@
 "use strict";
 
-AOS.init();
+var links = document.getElementsByTagName("a");
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+  for (var _iterator = links[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+    var i = _step.value;
+
+    if (i.href.startsWith("https://salebot.site/r/")) {
+      if (i.href.split("?").length > 1) {
+        i.href += "&" + location.search.split("?")[1];
+      } else {
+        i.href += location.search;
+      }
+    }
+  }
+} catch (err) {
+  _didIteratorError = true;
+  _iteratorError = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+      _iterator["return"]();
+    }
+  } finally {
+    if (_didIteratorError) {
+      throw _iteratorError;
+    }
+  }
+}
+
+var links = document.getElementsByTagName("a");
+var cookies = ['_fbc', '_fbp', '_ga', '_ym_uid', 'roistat_visit'];
+var cookies_string = '';
+cookies.forEach(function (cookie_name) {
+  var cookie_val = get_cookie(cookie_name);
+
+  if (cookie_val !== undefined) {
+    cookies_string += (cookies_string != '' ? "&" : '') + cookie_name + "='" + cookie_val + "'";
+  }
+});
+var _iteratorNormalCompletion2 = true;
+var _didIteratorError2 = false;
+var _iteratorError2 = undefined;
+
+try {
+  for (var _iterator2 = links[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+    var _i = _step2.value;
+
+    if (_i.href.startsWith("https://salebot.site/r/")) {
+      if (_i.href.split("?").length > 1) {
+        _i.href += "&" + cookies_string;
+      } else {
+        _i.href += "?" + cookies_string;
+      }
+    }
+  }
+} catch (err) {
+  _didIteratorError2 = true;
+  _iteratorError2 = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion2 && _iterator2["return"] != null) {
+      _iterator2["return"]();
+    }
+  } finally {
+    if (_didIteratorError2) {
+      throw _iteratorError2;
+    }
+  }
+}
+
+function get_cookie(name) {
+  var matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
